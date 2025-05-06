c5f8d07a0c28084c24fcdf2fb1ccf87a4797fb77
RadFelis-catus <radhouchehr@gmail.com>
2025-05-06 12:48:30 -0500

    Day 3

--- Rush Hour/Assets/Scripts/MoveScript.cs
+++ Rush Hour/Assets/Scripts/MoveScript.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*The purpose of this script is to move a vehicle up, down, left, or right 
+dependings on its orientation. Movement will occur when the player clicks and drags a vehicle in the intended direction.*/
 public class MoveScript : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Vector2 mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+    }
+
+    public void Move() {
+        if (Input.GetMouseButtonDown(0)) {
+            mousePos = Input.mousePosition;
+            Debug.Log("Mouse pressed down at: " + mousePos);
+        }
+    }
+
+    void OnMouseDrag() {
         
     }
 }
