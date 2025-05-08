85ef6ae3d47f4a39a93be4c18eeff951146399dc
RadFelis-catus <radhouchehr@gmail.com>
2025-05-08 12:47:52 -0500

    Day 4

--- Rush Hour/Assets/Scripts/MoveScript.cs
+++ Rush Hour/Assets/Scripts/MoveScript.cs
@@ -7,30 +7,54 @@ dependings on its orientation. Movement will occur when the player clicks and dr
 public class MoveScript : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Vector3 screenPoint;
-    private Vector3 offset;
-
+    [SerializeField] Vector3 screenPoint;
+    [SerializeField] Vector3 curScreenPoint;
+    [SerializeField] Vector3 curPosition;
+    [SerializeField]Vector3 offset;
+    [SerializeField] Vector3 origin;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float maxDistance;
+    [SerializeField] bool canMove;
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        canMove = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetMouseButton(0)) {
+            //Debug.Log("The Left Mouse Button is being held down");
+            Drag(); 
+        }
     }
 
-    //called when mouse is clicked
-    void OnMouseDown() {
-        Debug.Log("Mouse clicked on: " + gameObject.name);
-        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position); //transforms world position of gameObject into screen coords
-        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z)); //offset = distance of gameObject from camera
-    }
+    void Drag() {
+        //Debug.Log("Drag Method was called");
+        
+        origin = transform.position;
+        direction = transform.TransformDirection(Vector3.forward);
+        maxDistance = 0.5f;
+        if (Physics.Raycast(origin, direction, maxDistance)) {
+            Debug.Log("There is something in front of the object");
+            canMove = false;
+        }
+        Debug.Log("canMove is: " + canMove);
 
-    void OnMouseDrag() {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //finds screen coords of cursor
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset; //determines world position of cursor, then adds distance from camera
-        transform.position = curPosition; //moves gameObject to cursor position
+
+        /*
+        if (canMove) {
+            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+            curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //finds screen coords of cursor
+            curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset; //determines world position of cursor, then adds distance from camera
+            gameObject.transform.position = curPosition; //moves gameObject to cursor position
+        }
+        */
+        
+
+        
     }
 }
