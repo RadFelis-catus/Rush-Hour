afd5c3552f7ecb764d966e04d78e49880cf29eab
RadFelis-catus <radhouchehr@gmail.com>
2025-05-12 12:48:32 -0500

    Day 6

--- Rush Hour/Assets/Scripts/MoveScript.cs
+++ Rush Hour/Assets/Scripts/MoveScript.cs
@@ -8,20 +8,23 @@ public class MoveScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector3 curScreenPoint;
-    [SerializeField] Vector3 curPosition;
+    private Vector3 curPosition;
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        if (Input.GetMouseButton(0)) {
-            Debug.Log("Mouse held down");
+        /*if (Input.GetMouseButton(0)) {
+            //Debug.Log("Mouse held down");
+
             Vector3 mousePos = Input.mousePosition;
-            Ray ray = Camera.main.ScreenPointToRay(mousePos);
             RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+            Physics.Raycast(ray, out hit);
             if (Physics.Raycast(ray, out hit)) {
                 Debug.Log("Mouse is on a collider");
                 if(hit.transform.CompareTag("Vehicle") || hit.transform.CompareTag("Player")) {
@@ -30,6 +33,27 @@ public class MoveScript : MonoBehaviour
                     curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
                     gameObject.transform.position = curPosition;
                }
+            }
+        }
+        */
+    }
+
+    void OnMouseDrag() {
+        //Debug.Log("OnMouseDrag called");
+        if(transform.CompareTag("Vehicle") || transform.CompareTag("Player")) {
+            //Debug.Log("Mouse is on the corrrect collider");
+            if (rb.constraints == RigidbodyConstraints2D.FreezePositionX) {
+                //if(rigidbody.constraints==RigidbodyConstraints.FreezeAll)print("Frozen");
+
+                curScreenPoint = new Vector3(0, Input.mousePosition.y, 10);
+                curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+                transform.position = curPosition;
+            }else if (rb.constraints == RigidbodyConstraints2D.FreezePositionY) {
+                curScreenPoint = new Vector3(Input.mousePosition.x, 0, 10);
+                curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+                transform.position = curPosition;
+            }else {
+                Debug.Log("This object is not constrained");
             }
         }
     }
